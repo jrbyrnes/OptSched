3f3054f4226086be57b1e8ad306c658f9a1b1324
jeff <jrbyrnes1989@gmail.com>
2021-02-25 15:06:03 -0800

    benchmark searching

--- lib/Scheduler/sched_basic_data.cpp
+++ lib/Scheduler/sched_basic_data.cpp
@@ -1049,9 +1049,9 @@ bool SchedRange::TightnLwrBoundRcrsvly(DIRECTION dir, InstCount newBound,
       InstCount nghbrNewBound = newBound + edgLbl;
 
      if (nghbrNewBound > nghbr->GetCrntLwrBound(dir, SolverID)) {
-       if (SolverID == 2) {
-          Logger::Log((Logger::LOG_LEVEL) 4, false, "need to tightn nghbr %d to LB %d (currently %d)",nghbr->GetNum(), nghbrNewBound, nghbr->GetCrntLwrBound(dir, SolverID));
-       }
+       //if (SolverID == 2) {
+       //   Logger::Log((Logger::LOG_LEVEL) 4, false, "need to tightn nghbr %d to LB %d (currently %d)",nghbr->GetNum(), nghbrNewBound, nghbr->GetCrntLwrBound(dir, SolverID));
+       //}
         //Logger::Info("nghbr->GetNum() %d", nghbr->GetNum());
         //Logger::Info("nghbr->IsScheduld(SolverID) %d", nghbr->IsSchduld(SolverID));
         bool nghbrFsblty = nghbr->TightnLwrBoundRcrsvly(
