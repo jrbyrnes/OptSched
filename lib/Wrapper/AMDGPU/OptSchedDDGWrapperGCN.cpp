644e0e54ac8b54e0be902581c00c0e5f24cfb133
jeff <jrbyrnes1989@gmail.com>
2022-02-16 16:48:50 -0800

    fix bug

--- lib/Wrapper/AMDGPU/OptSchedDDGWrapperGCN.cpp
+++ lib/Wrapper/AMDGPU/OptSchedDDGWrapperGCN.cpp
@@ -102,7 +102,7 @@ collectVirtualRegDefs(const MachineInstr &MI, const LiveIntervals &LIS,
                       const MachineRegisterInfo &MRI) {
   SmallVector<RegisterMaskPair, 8> Res;
   Logger::Info("inst has %d defs", MI->getNumDefs());
-  Logger::Info("inst has %d operands", MI->getNumOperands());
+  Logger::Info("inst has %d operands", MI.getNumOperands());
   for (const auto &MO : MI.defs()) {
     if (!MO.isReg() || !MO.getReg().isVirtual() ||
         MO.isDead())
@@ -139,7 +139,7 @@ collectLiveSubRegsAtInstr(const MachineInstr *MI, const LiveIntervals *LIS,
       continue;
     auto LiveMask = getLiveLaneMask(Reg, SI, *LIS, MRI);
     if (LiveMask.any()) {
-      Logger::Info("found Reg %u with mask %d", Reg.id(), LiveMask.getAsInteger())
+      Logger::Info("found Reg %u with mask %d", Reg.id(), LiveMask.getAsInteger());
       Res.emplace_back(Reg, LiveMask);
     }
   }
