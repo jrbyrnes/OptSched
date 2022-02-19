1a2b3e52ffd3c06e077fab6cc9c7951b72b683eb
jeff <jrbyrnes1989@gmail.com>
2022-02-18 17:09:55 -0800

    set SI MF through DDG Contex

--- lib/Scheduler/data_dep.cpp
+++ lib/Scheduler/data_dep.cpp
@@ -840,7 +840,7 @@ SchedInstruction *DataDepGraph::CreateNode_(
     maxFileSchedOrder_ = fileSchedOrder;
 
 
-  newInstPtr->setMF(MF_);
+  newInstPtr->setMF(Contex->MF);
   insts_[instNum] = newInstPtr;
 
   return newInstPtr;
