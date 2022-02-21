681b3a941ff3eb798e879f4c1ff7f19c436ae8c4
jeff <jrbyrnes1989@gmail.com>
2022-02-21 13:05:20 -0800

    increase reg file size (regMask overuses regs)

--- lib/Wrapper/OptSchedMachineWrapper.cpp
+++ lib/Wrapper/OptSchedMachineWrapper.cpp
@@ -101,12 +101,12 @@ void OptSchedMachineModel::convertMachineModel(
   if (mdlName_ == "amdgcn") {
     RegTypeInfo SGPR32;
     SGPR32.name = "SGPR32";
-    SGPR32.count = 80;
+    SGPR32.count = 80*2;
     registerTypes_.push_back(SGPR32);
 
     RegTypeInfo VGPR32;
     VGPR32.name = "VGPR32";
-    VGPR32.count = 24;
+    VGPR32.count = 24*2;
     registerTypes_.push_back(VGPR32);
   } else {
     const auto *TRI = dag.TRI;
